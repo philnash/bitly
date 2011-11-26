9f88e2aaee676586a3ec140b5483adcb1283e541
Syl Turner <syl.turner@hannonhill.com>
2011-11-26 10:51:06 -0500

    Added a Config class to set the API version, API key, and username from an initializer. When setting the values that way, you get the bitly client through `Bitly.client`

---
+++ lib/bitly/config.rb
@@ -0,0 +1,25 @@
+module Bitly
+  module Config
+    
+    # bitly client options
+    OPTION_KEYS = [
+      :username,
+      :api_key,
+      :api_version
+    ]
+  
+    attr_accessor *OPTION_KEYS
+  
+    def configure
+      yield self
+      self
+    end
+  
+    def options
+      options = {}
+      OPTION_KEYS.each{|key| options[key] = send(key)}
+      options
+    end
+
+  end
+end
