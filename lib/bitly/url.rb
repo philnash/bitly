fe3ab8a9e547904683754b6d2d6ae2a53531cf88
Phil Nash <philnash@gmail.com>
2017-02-25 17:29:50 +1100

    Fix instance variable warnings

--- lib/bitly/url.rb
+++ lib/bitly/url.rb
@@ -1,26 +1,26 @@
 module Bitly
-  
+
   class Url
     include Bitly::Utils
-    
+
     attr_accessor :long_url, :short_url, :hash, :user_hash
     VARIABLES = ['long_url', 'short_url', 'hash', 'user_hash']
-    
+
     def initialize(login,api_key,obj=nil)
       unless obj.nil?
         raise BitlyError.new(obj['errorMessage'],obj['errorCode']) if obj['statusCode'] == "ERROR"
         instance_variablise(obj, VARIABLES)
-        @info = obj[:info] if obj[:info]
-        @stats = obj[:stats] if obj[:stats]
+        @info = obj.fetch(:info, nil)
+        @stats = obj.fetch(:stats, nil)
       end
       @login = login
       @api_key = api_key
       raise ArgumentError.new("Please provide a login and api_key") if @login.nil? || @api_key.nil?
     end
-    
+
     def shorten(opts = {})
-      return @short_url if @short_url
-      unless @long_url.nil?
+      return @short_url if defined? @short_url
+      if defined? @long_url
         request = create_url("shorten", :longUrl => @long_url, :history => (opts[:history] ? 1 : nil))
         result = get_result(request)[@long_url.gsub(/\/$/,'')]
         if result['statusCode'] == "ERROR"
@@ -33,11 +33,11 @@ module Bitly
         raise ArgumentError.new("You need a long_url in order to shorten it")
       end
     end
-    
+
     def expand
-      return @long_url if @long_url
-      unless !(@short_url || @hash)
-        unless @hash
+      return @long_url if defined? @long_url
+      unless !(short_url || hash)
+        unless defined? @hash
           @hash = create_hash_from_url(@short_url)
         end
         request = create_url("expand", :hash => @hash)
@@ -52,15 +52,15 @@ module Bitly
         raise ArgumentError.new("You need a short_url or a hash in order to expand it")
       end
     end
-    
+
     def info
       if @info.nil?
-        if @hash
+        if defined? @hash
           request = create_url "info", :hash => @hash
           result = get_result(request)[@hash]
           instance_variablise(result, VARIABLES)
           @info = result
-        elsif @short_url
+        elsif defined? @short_url
           @hash = create_hash_from_url(@short_url)
           request = create_url "info", :hash => hash
           result = get_result(request)[hash]
@@ -74,12 +74,12 @@ module Bitly
         @info
       end
     end
-    
+
     def stats
       if @stats.nil?
-        if @hash
+        if defined? @hash
           request = create_url "stats", :hash => @hash
-        elsif @short_url
+        elsif defined? @short_url
           @hash = create_hash_from_url(@short_url)
           request = create_url "stats", :hash => @hash
         else
@@ -90,11 +90,11 @@ module Bitly
         @stats
       end
     end
-    
+
     def bitly_url
       @short_url.nil? ? nil : @short_url.gsub(/j\.mp/,'bit.ly')
     end
-    
+
     def jmp_url
       @short_url.nil? ? nil : @short_url.gsub(/bit\.ly/,'j.mp')
     end
