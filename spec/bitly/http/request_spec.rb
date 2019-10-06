faaf4e37138450b626f35e3291acdb49e369b1e1
Phil Nash <philnash@gmail.com>
2019-10-06 11:41:10 +1100

    A request's URI should be made of all parameters if it is a GET.

--- spec/bitly/http/request_spec.rb
+++ spec/bitly/http/request_spec.rb
@@ -1,24 +1,26 @@
 RSpec.describe Bitly::HTTP::Request do
-  let :uri { URI.parse("http://example.com") }
+  let(:uri) { URI.parse("http://example.com") }
 
-  it "is initialized with a URI and a method" do
-    request = Bitly::HTTP::Request.new(uri: uri, method: "GET")
+  it "is initialized with a URI and the method is GET by default" do
+    request = Bitly::HTTP::Request.new(uri: uri)
     expect(request.uri).to eq(uri)
     expect(request.method).to eq("GET")
+  end
+
+  it "is initialized with a URI and a method" do
+    request = Bitly::HTTP::Request.new(uri: uri, method: "POST")
+    expect(request.uri).to eq(uri)
+    expect(request.method).to eq("POST")
   end
 
   it "is initialized with a URI and a method and an optional params hash" do
-    request = Bitly::HTTP::Request.new(uri: uri, method: "GET", params: { foo: "bar" })
-    expect(request.uri).to eq(uri)
-    expect(request.method).to eq("GET")
-    expect(request.params).to eq({ foo: "bar" })
+    request = Bitly::HTTP::Request.new(uri: uri, method: "GET", params: { "foo" => "bar" })
+    expect(request.params).to eq({ "foo" => "bar" })
   end
 
   it "is initialized with a URI and a method and an optional headers hash" do
-    request = Bitly::HTTP::Request.new(uri: uri, method: "GET", headers: { foo: "bar" })
-    expect(request.uri).to eq(uri)
-    expect(request.method).to eq("GET")
-    expect(request.headers).to eq({ foo: "bar" })
+    request = Bitly::HTTP::Request.new(uri: uri, method: "GET", headers: { "foo" => "bar" })
+    expect(request.headers).to eq({ "foo" => "bar" })
   end
 
   it "expects the uri to be an object of type URI" do
@@ -44,4 +46,18 @@ RSpec.describe Bitly::HTTP::Request do
       Bitly::HTTP::Request.new(uri: uri, method: "GET", headers: "wrong")
     }.to raise_error(ArgumentError)
   end
+
+  describe "when the method is GET" do
+    it "adds the params to the URL query string" do
+      request = Bitly::HTTP::Request.new(uri: uri, method: "GET", params: { "foo" => "bar" })
+      expect(request.uri.to_s).to eq("http://example.com?foo=bar")
+    end
+
+    it "adds the params to the existing URL query string" do
+      uri.query = "foo=baz"
+      request = Bitly::HTTP::Request.new(uri: uri, method: "GET", params: { "foo" => "bar" })
+      expect(request.uri.to_s).to eq("http://example.com?foo=baz&foo=bar")
+    end
+  end
+
 end
