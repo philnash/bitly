ece533ddde3e68975b46de8afa793756a71fe49f
Phil Nash <philnash@gmail.com>
2019-10-27 18:20:00 +1100

    Adds last response to organization object

---
+++ lib/bitly/api/list.rb
@@ -0,0 +1,17 @@
+module Bitly
+  module API
+    class List
+      include Base
+      include Enumerable
+
+      def initialize(items, response)
+        @items = items
+        @response = response
+      end
+
+      def each
+        @items.each { |item| yield item }
+      end
+    end
+  end
+end
--- lib/bitly/api/organization.rb
+++ lib/bitly/api/organization.rb
@@ -1,29 +1,36 @@
 # frozen_string_literal: true
+require_relative './base.rb'
+require_relative './list.rb'
 
 module Bitly
   module API
     class Organization
+      include Base
+
+      class List < Bitly::API::List ; end
+
       def self.list(client)
         response = client.request(path: '/organizations')
-        response.body['organizations'].map { |org| Organization.new(org) }
+        List.new(response.body['organizations'].map { |org| Organization.new(org) }, response)
       end
 
       def self.fetch(client, guid)
         response = client.request(path: "/organizations/#{guid}")
-        Organization.new(response.body)
+        Organization.new(response.body, response)
       end
 
       ATTRIBUTES = [:name, :guid, :is_active, :tier, :tier_family, :tier_display_name, :role, :bsds]
       TIME_ATTRIBUTES = [:created, :modified]
       attr_reader *(ATTRIBUTES + TIME_ATTRIBUTES)
 
-      def initialize(opts)
+      def initialize(opts, response=nil)
         ATTRIBUTES.each do |attr|
           instance_variable_set("@#{attr}", opts[attr.to_s]) if opts[attr.to_s]
         end
         TIME_ATTRIBUTES.each do |attr|
           instance_variable_set("@#{attr}", Time.parse(opts[attr.to_s])) if opts[attr.to_s]
         end
+        @response = response
       end
     end
   end
