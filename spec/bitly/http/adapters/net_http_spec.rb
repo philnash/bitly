097200082130c2cb858813089250d6166918b425
Phil Nash <philnash@gmail.com>
2020-01-01 17:03:48 +1100

    Adds test for NetHTTP adapter

--- spec/bitly/http/adapters/net_http_spec.rb
+++ spec/bitly/http/adapters/net_http_spec.rb
@@ -1,5 +1,32 @@
 # frozen_string_literal: true
 
 RSpec.describe Bitly::HTTP::Adapters::NetHTTP do
+  it "makes get requests using a Bitly::HTTP::Request object" do
+    stub_request(:get, "https://example.com/")
+      .to_return(body: { "success" => true }.to_json)
+    request = Bitly::HTTP::Request.new(uri: URI.parse("https://example.com/"))
+    adapter = Bitly::HTTP::Adapters::NetHTTP.new
+    response = adapter.request(request)
+    expect(response).to eq(["200", { "success" => true }.to_json, {}, true])
+  end
 
+  it "makes post requests using a Bitly::HTTP::Request object including a body" do
+    stub_request(:post, "https://example.com/")
+      .with(body: { "hello" => "world" }.to_json)
+      .to_return(body: { "success" => true }.to_json)
+    request = Bitly::HTTP::Request.new(uri: URI.parse("https://example.com/"), method: "POST", params: { "hello" => "world"})
+    adapter = Bitly::HTTP::Adapters::NetHTTP.new
+    response = adapter.request(request)
+    expect(response).to eq(["200", { "success" => true }.to_json, {}, true])
+  end
+
+  it "makes post requests using a Bitly::HTTP::Request object including headers" do
+    stub_request(:post, "https://example.com/")
+      .with(headers: { "UserAgent" => "BitlyTest" })
+      .to_return(body: { "success" => true }.to_json)
+    request = Bitly::HTTP::Request.new(uri: URI.parse("https://example.com/"), method: "POST", headers: { "UserAgent" => "BitlyTest" })
+    adapter = Bitly::HTTP::Adapters::NetHTTP.new
+    response = adapter.request(request)
+    expect(response).to eq(["200", { "success" => true }.to_json, {}, true])
+  end
 end
