f721aa20c49197741cd18d6c12860d6f0668828f
Phil Nash <philnash@gmail.com>
2020-01-02 16:58:14 +1100

    Adds comment docs

--- lib/bitly/api/client.rb
+++ lib/bitly/api/client.rb
@@ -2,14 +2,48 @@
 
 module Bitly
   module API
+    ##
+    # The class that all the API requests are made through. The
+    # Bitly::API::Client is authorized with an OAuth token and takes care of
+    # setting the correct path, parameters and headers when making requests to
+    # the API.
+    #
+    # This class will be used the most and includes short cut methods to request
+    # the popular objects in the system.
     class Client
       USER_AGENT = "Ruby Bitly/#{Bitly::VERSION}"
 
+      ##
+      # Creates a new Bitly::API::Client, authorized with an OAuth token and
+      # with an optional HTTP client.
+      #
+      # @example
+      #     client = Bitly::API::Client.new(token: oauth_token)
+      #
+      # @param http [Bitly::HTTP::Client] An HTTP client, you can pass your own
+      #     if you have a custom client you would like to use.
+      # @param token [String] An OAuth token for a user authorized with the API.
+      #
+      # @return [Bitly::API::Client]
       def initialize(http: Bitly::HTTP::Client.new, token:)
         @http = http
         @token = token
       end
 
+      ##
+      # Makes a request to the API by building up a Bitly::HTTP::Request and
+      # using the HTTP client to make that request and return a
+      # Bitly::HTTP::Response.
+      #
+      # @example
+      #     client.request("/shorten", method: "POST", params: { long_url => "https://example.com" })
+      #
+      # @param path [String] The resource path
+      # @param method [String] The HTTP method to use to request the path
+      # @param params [Hash<String, String>] The parameters to pass to the API
+      # @param headers [Hash<String, String>] Custom headers for the request
+      #
+      # @return [Bitly::HTTP::Response]
       def request(path:, method: 'GET', params: {}, headers: {})
         params = params.select { |k,v| !v.nil? }
         headers = default_headers.merge(headers)
@@ -19,22 +53,66 @@ module Bitly
         @http.request(request)
       end
 
+      ##
+      # Shortens a long URL
+      #
+      # @example
+      #     client.shorten(long_url: "http://example.com")
+      #
+      # @param long_url [String] The URL you want to shorten
+      # @param domain [String] The domain you want to shorten the URL with.
+      #     "bit.ly" by default.
+      # @param group_guid [String] The group you want shorten this URL under
+      #
+      # @return [Bitly::API::Bitlink]
       def shorten(long_url:, domain: nil, group_guid: nil)
         Bitlink.shorten(client: self, long_url: long_url, domain: domain, group_guid: group_guid)
       end
 
+      ##
+      # Lists organizations the authorized user can see.
+      #
+      # @example
+      #     organizations = client.organizations
+      #
+      # @return [Bitly::API::Organization::List]
       def organizations
         Organization.list(client: self)
       end
 
+      ##
+      # Fetch a particular organization.
+      #
+      # @example
+      #     organization = client.organization(guid)
+      #
+      # @param guid [String] The guid of the organization you want.
+      #
+      # @return [Bitly::API::Organization]
       def organization(guid)
         Organization.fetch(client: self, guid: guid)
       end
 
+      ##
+      # Lists groups the authorized user can see.
+      #
+      # @example
+      #     groups = client.groups
+      #
+      # @return [Bitly::API::Group::List]
       def groups(organization: nil)
         Group.list(client: self, organization: organization)
       end
 
+      ##
+      # Fetch a particular group.
+      #
+      # @example
+      #     group = client.group(guid)
+      #
+      # @param guid [String] The guid of the group you want.
+      #
+      # @return [Bitly::API::Group]
       def group(guid)
         Group.fetch(client: self, guid: guid)
       end
--- lib/bitly/api/organization.rb
+++ lib/bitly/api/organization.rb
@@ -4,35 +4,81 @@ require_relative './list.rb'
 
 module Bitly
   module API
+    ##
+    # An Organization is the top level of the Bitly user hierarchy. Both Users
+    # and Groups live within an organization.
     class Organization
       include Base
 
+      ##
+      # An Organization::List is a container for a list of organizations.
       class List < Bitly::API::List ; end
 
+      ##
+      # Get a list of organizations from the API. It receives an authorized
+      # `Bitly::API::Client` object and uses it to request the `/organizations`
+      # endpoint..
+      #
+      # @example
+      #     organization = Bitly::API::Organization.list(client: client)
+      #
+      # @param client [Bitly::API::Client] An authorized API client
+      #
+      # @return [Bitly::API::Organization::List]
       def self.list(client:)
         response = client.request(path: '/organizations')
         List.new(response.body['organizations'].map { |org| Organization.new(data: org, client: client) }, response)
       end
 
+      ##
+      # Retrieve an organization from the API. It receives an authorized
+      # `Bitly::API::Client` object and an organization guid and uses it to
+      #  request the `/organizations/:organization_guid` endpoint.
+      #
+      # @example
+      #     organization = Bitly::API::Organization.fetch(client: client, guid: guid)
+      #
+      # @param client [Bitly::API::Client] An authorized API client
+      # @param guid [String] An organization guid
+      #
+      # @return [Bitly::API::Organization]
       def self.fetch(client:, guid:)
         response = client.request(path: "/organizations/#{guid}")
         Organization.new(data: response.body, client: client, response: response)
       end
 
+      # @return [Array<Symbol>] The attributes the API returns for an
+      # organization
       def self.attributes
         [:name, :guid, :is_active, :tier, :tier_family, :tier_display_name, :role, :bsds]
       end
+      # @return [Array<Symbol>] The attributes the API returns that need to be
+      # converted to `Time` objects.
       def self.time_attributes
         [:created, :modified]
       end
       attr_reader(*(attributes + time_attributes))
 
+      ##
+      # Creates a new `Bitly::API::Organization` object
+      #
+      # @example
+      #     organization = Bitly::API::Organization.new(data: org_data, client: client)
+      #
+      # @param data [Hash<String, String|Boolean>] Data returned from the API
+      #     about the organization
+      # @param client [Bitly::API::Client] An authorized API client
+      # @param respomnse [Bitly::HTTP::Response] The response object from an API
+      #     call
+      #
+      # @return [Bitly::API::Organization]
       def initialize(data:, client:, response: nil)
         assign_attributes(data)
         @client = client
         @response = response
       end
 
+      # @return [Bitly::API::Group::List]
       def groups
         @groups ||= Group.list(client: @client, organization: self)
       end
